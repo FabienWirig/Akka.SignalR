001ad3d8f591496dc4a9fd463cad2e1af6dcc4c6
FabienW <fabien.wirig@gmail.com>
2017-03-09 23:13:16 +0100

    Save states in Console Application

--- Game.Web/Models/GameActorSystem.cs
+++ Game.Web/Models/GameActorSystem.cs
@@ -14,9 +14,11 @@ namespace Game.Web.Models
         {
             _gameEventsPusher = new SignalRGameEventPusher();
 
-            ActorSystem = Akka.Actor.ActorSystem.Create("GameSystem");
+            ActorSystem = ActorSystem.Create("GameSystem");
 
-            ActorReferences.GameController = ActorSystem.ActorOf<GameControllerActor>();
+            ActorReferences.GameController = ActorSystem.ActorSelection("akka.tcp://GameSystem@127.0.0.1:8091/user/GameController")
+                .ResolveOne(TimeSpan.FromSeconds(3))
+                .Result;
 
             ActorReferences.SignalRBridge = ActorSystem.ActorOf(
                 Props.Create(() => new SignalRBridgeActor(_gameEventsPusher, ActorReferences.GameController)),
